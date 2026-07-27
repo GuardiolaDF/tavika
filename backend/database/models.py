e8cbf5b32b9726b3f4f729cde411be5db71c8bb1
Tequila Pontiac Dev <dev@tequila-pontiac.local>
2026-07-27 17:21:03 -0300

    Update models for national scope

--- backend/database/models.py
+++ backend/database/models.py
@@ -22,7 +22,9 @@ class Colegio(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, index=True)
+    provincia = Column(String, index=True)
     distrito = Column(String, index=True)
+    sector = Column(String, index=True) # Privado, Estatal, etc
     nivel = Column(String, index=True) # Jardin, Primaria, Secundaria, etc
     email = Column(String, nullable=True)
     origen = Column(String) # Origen del dato (abc, web, etc)
